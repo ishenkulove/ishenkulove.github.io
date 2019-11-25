f64fd7346eee965099177b8f9e2b097885a46e49
ishenkulove <48785355+ishenkulove@users.noreply.github.com>
2019-11-25 10:56:28 +0600

    Add files via upload

---
+++ tours/scripts/_slick-slider.js
@@ -0,0 +1,27 @@
+$(".slider").slick({
+  centerMode: true,
+  infinite: true,
+  speed: 1000,
+  centerPadding: "60px",
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 1
+      }
+    }
+  ]
+});
